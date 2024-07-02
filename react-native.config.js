063bdabc6eeb02d990322725c47cbf60034796f5
Nizam <ramazanov570633@gmail.com>
2024-07-02 12:06:27 +0400

    advance folder

--- react-native.config.js
+++ react-native.config.js
@@ -6,3 +6,19 @@ module.exports={
     assets:['./src/assets/fonts/']
   }
   
+
+  // module.exports = {
+  //   project: {
+  //     ios: {
+  //       sourceDir: './ios',
+  //     },
+  //   },
+  //   assets: ['./src/ui/assets/fonts'],
+  //   dependencies: {
+  //     'react-native-vector-icons': {
+  //       platforms: {
+  //         ios: null,
+  //       },
+  //     },
+  //   },
+  // };
