e37035c87a30f67bc3b32a43bac6c91634ff5e0e
Nizam <ramazanov570633@gmail.com>
2024-08-27 16:51:14 +0400

    test 21

---
+++ react-native.config.js
@@ -0,0 +1,24 @@
+module.exports={
+    project: {
+      ios: {},
+      android: {},
+    },
+    assets:['./src/assets/fonts/']
+  }
+  
+
+  // module.exports = {
+  //   project: {
+  //     ios: {
+  //       sourceDir: './ios',
+  //     },
+  //   },
+  //   assets: ['./src/ui/assets/fonts'],
+  //   dependencies: {
+  //     'react-native-vector-icons': {
+  //       platforms: {
+  //         ios: null,
+  //       },
+  //     },
+  //   },
+  // };
